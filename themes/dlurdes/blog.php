77ac9599acfbf3b6826f58dd5c398eb6a7037791
casa <casa@DESKTOP-FJT5F0L>
2018-08-27 18:11:59 -0300

    Create page blog

---
+++ themes/dlurdes/blog.php
@@ -0,0 +1,55 @@
+<main class="main_content container">
+    <div class="thumb-cardapio">
+        <img src="<?= REQUIRE_PATH; ?>/img/fundo-cardapio.jpg" alt=""/>
+        <img src="<?= REQUIRE_PATH; ?>/img/" alt=""/>
+        <div class="clear"></div>
+    </div>
+</main>
+<main class="container">
+    <div class="banner-mobile">         
+        <img src="<?= REQUIRE_PATH; ?>/img/banner-mob-card-1.jpg"  class="mobi-700" alt="Banner" title="Cardápio D'Lurdes"/>
+        <img src="<?= REQUIRE_PATH; ?>/img/banner-mob-top-320-1.jpg" class="mobi-400"  alt="Banner" title="Cardápio D'Lurdes"/>
+        <div class="mirros-color"></div>
+    </div>
+</main>
+
+<main class="main-blog container">
+    <section class="sec-blog">
+        <div class="header-title txt-cente content">
+            <h1 class="conf-h1 space-mb-a">Blog</h1>
+            <p class="tagline cl-rgba-cinza space-bottom-d">
+                Blog D'Lurdes com as melhores informações e dicas.
+            </p>
+            <div class="clear"></div>
+        </div> 
+
+        <div class="content">
+            <?php
+            for ($i = 1; $i <= 12; $i++):
+                ?>
+                <div class="box-thum-blog">
+                    <a href="#">
+                        <div class="thumb-blog">
+                            <img src="<?= REQUIRE_PATH; ?>/img/our-staff-image.jpg" alt=""/>
+                            <div class="layer-blog" title="Artigo do Blog">
+                                <i class="fa fa-camera-retro"></i>
+                            </div>
+                        </div>   
+                    </a>      
+                    <div class="desc-thumb-blog">
+                        <span><i class="fa fa-clock-o"></i> AGOSTO 27/2018 <a href="#" class="next-coments" title="Comentários"><i class="fa fa-comments"></i></a></span>
+                        <h2>Dicas de um boa Alimentação</h2>
+                        <p class="space-mb-b">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam semper leo eget sapien ultrices...
+                        </p>
+                        <a href="#" class="next-article" title="Continuar lendo" >Continue lendo <i class="fa fa-arrow-circle-o-right"></i></a>
+                    </div>
+
+                </div>
+                <?php
+            endfor;
+            ?>
+        </div>
+    </section>
+
+</main>
--- themes/dlurdes/cardapio.php
+++ themes/dlurdes/cardapio.php
@@ -1,9 +1,19 @@
-<div class="thumb-cardapio container">
-    <img src="<?= REQUIRE_PATH; ?>/img/fundo-cardapio.jpg" alt=""/>
-    <div class="clear"></div>
-</div>
+<main class="main_content container">
+    <div class="thumb-cardapio">
+        <img src="<?= REQUIRE_PATH; ?>/img/fundo-cardapio.jpg" alt=""/>
+        <img src="<?= REQUIRE_PATH; ?>/img/" alt=""/>
+        <div class="clear"></div>
+    </div>
+</main>
+<main class="container">
+    <div class="banner-mobile">         
+        <img src="<?= REQUIRE_PATH; ?>/img/banner-mob-card-1.jpg"  class="mobi-700" alt="Banner" title="Cardápio D'Lurdes"/>
+        <img src="<?= REQUIRE_PATH; ?>/img/banner-mob-top-320-1.jpg" class="mobi-400"  alt="Banner" title="Cardápio D'Lurdes"/>
+        <div class="mirros-color"></div>
+    </div>
+</main>
 
-<main class="main-cardapio container pd-forxfor">
+<main class="main-cardapio container">
     <section class="sec-cardapio">
         <div class="header-title txt-cente content">
             <h1 class="conf-h1 space-mb-a">Cardápio</h1>
@@ -39,7 +49,7 @@
                     <a href="#" class="btn-black ft-sz-two">Veja Mais</a>
                 </div>     
             </div>
-              <div class="clear"></div>  
+            <div class="clear"></div>  
         </article>
 
         <article class="our-services container">
@@ -54,26 +64,95 @@
 
             <div class="box-our-services content">
                 <?php
-                for($i=1;$i<=8;$i++):
+                for ($i = 1; $i <= 8; $i++):
+                    ?>
+                    <div class="box-our-serv">
+                        <div class="thumb-icon-serv">
+                            <i>
+                                <img src="<?= REQUIRE_PATH; ?>/img/wine-glass.png" alt=""/>
+                            </i>
+                        </div>     
+                        <div class="desc-our-serv txt-cente pd-topxbottom">
+                            <h2 class="space-mb">JANTAR</h2>
+                            <p class="txt-just space-mb-d">
+                                Integer posuere erat a ante venenatis dapibus
+                                posuere velit aliquet. Cras mattis consectetur purus sit amet fermentum.                            
+                            </p>
+                            <a href="#">Leia mais <i class="fa fa-arrow-circle-o-right"></i></a>
+                        </div>
+                    </div>
+                    <?php
+                endfor;
                 ?>
-                <div class="box-our-serv">
-                    <div class="thumb-icon-serv">
-                        <i>
-                            <img src="<?= REQUIRE_PATH; ?>/img/wine-glass.png" alt=""/>
-                        </i>
-                    </div>     
-                    <div class="desc-our-serv txt-cente pd-topxbottom">
-                        <h2 class="space-mb">JANTAR</h2>
-                        <p class="txt-just space-mb-d">
-                            Integer posuere erat a ante venenatis dapibus
-                            posuere velit aliquet. Cras mattis consectetur purus sit amet fermentum.                            
-                        </p>
-                        <a href="#">Leia mais <i class="fa fa-arrow-circle-o-right"></i></a>
+                <div class="clear"></div>
+            </div>
+        </article>
+
+        <!-------------------------------------PACKAGES OF THE DAY ------------------------------------->    
+        <article class="packages container">
+            <div class="content">
+                <div class="header-title txt-cente">
+                    <h1 class="conf-h1 space-mb-a">PRATOS DA SEMANA</h1>
+                    <p class="tagline">
+                        Our photography packages provide hours of coverage,
+                        capturing all the essential elements of your event
+                        professionally & creatively.
+                    </p>
+                </div>
+                <div class="card-plates">
+                    <div class="box-card-plates box txt-cente">
+                        <div class="box-plates">
+                            <div class="box-pizza cl-branca">
+                                <h2 class="fw-a"> RODIZIO DE PIZZA</h2>
+                                <p>  <sup class="ft-sz-zr-nine"> R$</sup> <strong class="ft-sz-three-fv fw-b">50,00</strong> </p>
+                                <p class="ft-sz-zr-nine">ATÉ A 00:00 Horas</p>
+                            </div>
+                            <div class="box-desc-plates">
+                                <h3>Pizzaria</h3>
+                                <p class="space-mb-c">
+                                    Aqui você participa e degusta
+                                    da melhor pizza de Brasília a
+                                    semana toda.
+                                </p>
+                                <a href="#" class="btn-black">VEJA MAIS</a>
+                            </div>
+                        </div>
+
+                        <div class="box-plates">
+                            <div class="box-lunch cl-branca">
+                                <h2 class="fw-a"> ALMOÇO</h2>
+                                <p>  <sup class="ft-sz-zr-nine"> R$</sup> <strong class="ft-sz-three-fv fw-b">50,00</strong> </p>
+                                <p class="ft-sz-zr-nine">ATÉ A 00:00 Horas</p>
+                            </div>
+                            <div class="box-desc-plates ft-sz-one">
+                                <h3>Almoço</h3>
+                                <p class="space-mb-c">
+                                    Integer posuere erat a ante venenatis
+                                    dapibus posuere velit aliquet. Cras mattis
+                                    consectetur purus sit amet fermentum.
+                                </p>
+                                <a href="#" class="btn-black">VEJA MAIS</a>
+                            </div>
+                        </div>
+
+                        <div class="box-plates">
+                            <div class="box-dine cl-branca">
+                                <h2 class="fw-a"> JANTAR</h2>
+                                <p>  <sup class="ft-sz-zr-nine"> R$</sup> <strong class="ft-sz-three-fv fw-b">50,00</strong> </p>
+                                <p class="ft-sz-zr-nine">ATÉ A 00:00 Horas</p>
+                            </div>
+                            <div class="box-desc-plates">
+                                <h3>JANTAR</h3>
+                                <p class="space-mb-c">
+                                    Integer posuere erat a ante venenatis
+                                    dapibus posuere velit aliquet. Cras mattis
+                                    consectetur purus sit amet fermentum.
+                                </p>
+                                <a href="#" class="btn-black">VEJA MAIS</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <?php
-                 endfor;
-                ?>
                 <div class="clear"></div>
             </div>
         </article>
